1c33d08a6702791c507cb2121072ea8a3038dc80
konosuke.takeshima <konosuke.takeshima@openir.onmicrosoft.com>
2019-08-24 19:48:09 +0900

    modify index.js

--- index.js
+++ index.js
@@ -2,43 +2,32 @@
 const express = require('express');
 const app = express();
 
-// body to json
+// request.body to json
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-
-app.get('/', (req, res) => {
-    res.send('Simple REST API');
-});
-
-app.get('/sample-nodejs', (req, res) => {
-    res.send('Sample Node.js Application!!');
-});
-
 const courses = [
-    { id: 1, name: 'computer science'},
-    { id: 2, name: 'information technology'},
-    { id: 3, name: 'business intelligence'},
+    { id: 1, name: 'computer science' },
+    { id: 2, name: 'information technology' },
+    { id: 3, name: 'business intelligence' },
 ];
 
-app.get('/api/courses', (req, res) => {
+app.get('/sample-nodejs', (req, res) => {
     res.send(courses);
 });
 
-app.post('/api/courses/create', (req, res) => {
-    console.log(req.body.name)
+app.post('/sample-nodejs/add-course', (req, res) => {
     const course = {
         id: courses.length + 1,
         name: req.body.name
     };
     courses.push(course);
+    console.log(course)
     res.send(course);
 });
 
