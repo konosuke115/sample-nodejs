e86a0e55ebced4953d0c8a12f8fb7e712206c536
konosuke.takeshima <konosuke.takeshima@openir.onmicrosoft.com>
2019-08-24 15:00:20 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const app = express();
+
+// body to json
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
+app.get('/', (req, res) => {
+    res.send('Simple REST API');
+});
+
+const courses = [
+    { id: 1, name: 'computer science'},
+    { id: 2, name: 'information technology'},
+    { id: 3, name: 'business intelligence'},
+];
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.post('/api/courses/create', (req, res) => {
+    console.log(req.body.name)
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
